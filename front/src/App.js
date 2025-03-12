96de2a69102d57497e0aa72519d458bbbc8051af
chemsseddine1 <132996005+chemsseddine1@users.noreply.github.com>
2025-03-12 14:43:13 +0100

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -4,12 +4,14 @@ import Journal from "./Components/Journal";
 import Inventory from "./Components/Inventory";
 
 import Stock from "./Components/Stock";
+import LoginSignup from "./Components/LoginSignup";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Journal />} />
+        <Route path='/' element={<LoginSignup />} />
+        <Route path='/journal' element={<Journal />} />
         <Route path='/inventory' element={<Inventory />} />
         <Route path='/stock' element={<Stock />} />
       </Routes>
